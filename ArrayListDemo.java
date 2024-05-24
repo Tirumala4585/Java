bf4f568bae304198229107f5d874d893061acb8e
Tirumala4585 <150008283+Tirumala4585@users.noreply.github.com>
2024-05-24 15:29:27 +0530

    Add files via upload

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,26 +1,26 @@
-package Arraylist;
+package com.collections;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class ArrayListDemo {
-	public static void main(String []args) {
-		ArrayList<Integer> arrlist = new ArrayList<Integer>();
-		arrlist.add(10);
-		arrlist.add(20);
-		arrlist.add(30);
-		arrlist.add(40);
-		arrlist.add(50);
-		//System.out.println(arrlist);
-		//for(int arr:arrlist) {
-			//System.out.println(arr);
-		//}
-	    Iterator<Integer> it = arrlist.iterator();
-	    while(it.hasNext()) {
-	    	 System.out.println(it.next());
-	    	
+	public static void main(String []args)
+	   {
+	           List<Integer> list = new ArrayList<>();
+	           list.add(1);
+	           list.add(2);
+	           list.add(3);
+	           list.add(4);
+	           Iterator<Integer> itr = list.iterator();
+	           while(itr.hasNext())
+	           {
+	                 int i=itr.next();
+	                 if(i>=3) {
+	                	 itr.remove();
+	                 }
+	           }
+	         System.out.println("Removing last element:"+list);
 	    }
-	}
 
 }
-
