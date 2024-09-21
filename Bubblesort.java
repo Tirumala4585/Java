ab4178a817f5d146b24cb7b36649741806c95c0b
Tirumala4585 <150008283+Tirumala4585@users.noreply.github.com>
2024-09-21 23:48:52 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,35 +1,35 @@
 package com.stringprograms;
 
 public class Bubblesort {
-	public static void main(String []args) 
-	   {
-	       int[] arry = {6,0,3,5};
-	       int count = arry.length;
-	       int temp;
-	       System.out.println("Array elements before sorting");
-		   for(int i=0;i<count;i++)
-		   {
-		        System.out.print(arry[i]+" ");
-		   }
-		   System.out.println();   
-	       for(int i=1;i<count;i++)
-	       {
-	          for(int j=0;j<count-1;j++)
-	          {
-	              if(arry[j]>arry[j+1])
-	              {
-	                  temp=arry[j];
-	                  arry[j]=arry[j+1];
-	                  arry[j+1]=temp;
-	               }
-	          }
-	      } 
-	      System.out.println("Array elements after sorting");
-	      for(int i=0;i<count;i++)
-	      {
-	          System.out.print(arry[i]+" ");
-	      }
-	  }
-
-
+    public static void sortting(int[] a)
+     {
+       int temp;
+       for(int i=0;i<a.length;i++)
+       {
+             for(int j=0;j<a.length-1;j++)
+             {
+               if(a[j]>a[j+1])
+               {
+                 temp=a[j];
+                 a[j]=a[j+1];
+                 a[j+1]=temp;
+               }
+             } 
+        }
+        System.out.println("After sorting an Array");
+        for(int i=0;i<a.length;i++)
+        {
+             System.out.println(a[i]);
+        }  
+    }
+    public static void main(String []args)
+    {
+          int[] a = {30,10,40,20,50};
+          System.out.println("Before sorting an Array");
+          for(int i=0;i<a.length;i++)  
+          {
+             System.out.println(a[i]);
+          }
+          sortting(a); 
+   }
 }
